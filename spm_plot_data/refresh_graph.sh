82146f8814ae0c0e25f3303a525ae645cf84e227
pipiland <nguyen.t.dang.minh@gmail.com>
2025-05-30 16:34:05 +0300

    extract service a param for script

--- spm_plot_data/refresh_graph.sh
+++ spm_plot_data/refresh_graph.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+service=${1:-jaeger}  # Default to 'redis' if no argument is provided
 current_timestamp=$(($(date +%s) * 1000))
 
-curl "http://localhost:16686/api/metrics/calls?service=customer&endTs=${current_timestamp}&lookback=21600000&quantile=0.95&ratePer=600000&spanKind=server&step=60000" \
+curl "http://localhost:16686/api/metrics/calls?service=${service}&endTs=${current_timestamp}&lookback=21600000&quantile=0.95&ratePer=600000&spanKind=server&step=60000" \
   -H 'Referer: http://localhost:16686/monitor' \
   -H 'Sec-Fetch-Dest: empty' \
   -H 'Sec-Fetch-Mode: cors' \
@@ -17,6 +18,71 @@ curl --request GET \
   --header 'content-type: application/json' \
   --header 'host: localhost:9200' \
   --header 'user-agent: vscode-restclient' \
-  --data '{"size": 0,"query": {"bool": {"filter": [{"term": {"process.serviceName": "customer"}},{"nested": {"path": "tags","query": {"bool": {"must": [{"term": {"tags.key": "span.kind"}},{"term": {"tags.value": "server"}}]}}}},{"range": {"startTimeMillis": {"gte": "now-6h-10m","lte": "now","format": "epoch_millis"}}}]}},"aggs": {"requests_per_bucket": {"date_histogram": {"field": "startTimeMillis","fixed_interval": "60s","min_doc_count": 0,"extended_bounds": {"min": "now-6h-10m","max": "now"}},"aggs": {"cumulative_requests": {"cumulative_sum": {"buckets_path": "_count"}},"rate_per_second": {"moving_fn": {"script": {"source": "if (values == null || values.length < params.window) {  return 0.0; } double windowSizeSeconds = params.window * params.interval_ms / 1000.0; double firstVal = values[0];\ndouble lastVal = values[values.length - 1]; return (lastVal - firstVal) / windowSizeSeconds;","lang": "painless","params": {"window": 10,"interval_ms": 60000}},"buckets_path": "cumulative_requests","window": 10}}}}}}' | jq . > es_data.json 
+  --data @- <<EOF | jq . > es_data.json
+{
+  "size": 0,
+  "query": {
+    "bool": {
+      "filter": [
+        { "term": { "process.serviceName": "${service}" } },
+        {
+          "nested": {
+            "path": "tags",
+            "query": {
+              "bool": {
+                "must": [
+                  { "term": { "tags.key": "span.kind" } },
+                  { "term": { "tags.value": "server" } }
+                ]
+              }
+            }
+          }
+        },
+        {
+          "range": {
+            "startTimeMillis": {
+              "gte": "now-6h-10m",
+              "lte": "now",
+              "format": "epoch_millis"
+            }
+          }
+        }
+      ]
+    }
+  },
+  "aggs": {
+    "requests_per_bucket": {
+      "date_histogram": {
+        "field": "startTimeMillis",
+        "fixed_interval": "60s",
+        "min_doc_count": 0,
+        "extended_bounds": {
+          "min": "now-6h-10m",
+          "max": "now"
+        }
+      },
+      "aggs": {
+        "cumulative_requests": {
+          "cumulative_sum": { "buckets_path": "_count" }
+        },
+        "rate_per_second": {
+          "moving_fn": {
+            "script": {
+              "source": "if (values == null || values.length < params.window) {  return 0.0; } double windowSizeSeconds = params.window * params.interval_ms / 1000.0; double firstVal = values[0]; double lastVal = values[values.length - 1]; return (lastVal - firstVal) / windowSizeSeconds;",
+              "lang": "painless",
+              "params": {
+                "window": 10,
+                "interval_ms": 60000
+              }
+            },
+            "buckets_path": "cumulative_requests",
+            "window": 10
+          }
+        }
+      }
+    }
+  }
+}
+EOF
 
 python plot-data.py
