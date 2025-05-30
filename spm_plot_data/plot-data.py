6deed6525c0b4656380db4316dc728d3ea9dfba1
pipiland <nguyen.t.dang.minh@gmail.com>
2025-05-30 15:34:10 +0300

    Plot missing requests_per_bucket value as 0

--- spm_plot_data/plot-data.py
+++ spm_plot_data/plot-data.py
@@ -41,12 +41,13 @@ def extract_call_rate_per_second_from_file(filepath: str) -> dict[int, float]:
 
     empty_value= 0.0  # Default value for empty buckets
     for bucket in buckets:
-        call_rate = bucket.get("rate_per_second", {}).get("value")
-        if call_rate is not None:
-            key = bucket.get("key")  # the timestamp key, e.g., 1747678380000
-            if key is not None:
-                result[key] = call_rate
-        else:
+        call_rate = bucket.get("rate_per_second", {}).get("value", 0.0)
+        
+        key = bucket.get("key")  # the timestamp key, e.g., 1747678380000
+        if key is not None:
+            result[key] = call_rate
+        
+        if call_rate is None:
             empty_value+= 1.0  # Increment empty value for each empty bucket        
     
     print(f"Empty buckets: {empty_value}")
